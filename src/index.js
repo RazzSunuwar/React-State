5f3fd58bab248d6abec905882bf247c95c5882b5
RazzSunuwar <razzkoinch5@gmail.com>
2020-03-06 09:35:43 +0545

    React State

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Example
+// Specifiy the state object in the constructor method:
+
+class Car extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {brand: "Ford"};
+    }
+    render(){
+        return(
+            <div>
+                <h1>My Car</h1>
+            </div>
+        );
+    };
+};
+
+ReactDOM.render(<Car />, document.getElementById('root'));
+
